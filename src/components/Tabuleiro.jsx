0622f0d23782b6caa6f0ad88ab237bb1149bf44d
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-09-29 14:17:39 -0300

    Jogo da Velha

--- src/components/Tabuleiro.jsx
+++ src/components/Tabuleiro.jsx
@@ -8,6 +8,11 @@ function Tabuleiro () {
     const [xProximo, setXProximo] = useState(true); // Para que quando renderizar o valor não seja perdido. A variável mantenha a memória (useState)
 
     function handleClick(i){
+        // Para que o mesmo quadrado não possa ser clicado duas vezes
+        if (quadrados[i]) {
+            return;
+        }
+
         // Criando cópias das constantes
         const nextQuadrado = quadrados.slice();
 
@@ -23,8 +28,19 @@ function Tabuleiro () {
         setXProximo(!xProximo);
     }
 
+    const venceu = Vencedor(quadrados);
+    let status;
+
+    if (venceu) {
+        status = "Vencedor é: " + venceu;
+    }
+    else {
+        status = "O próximo jogador é: " + (xProximo ? "X": "O");
+    }
+
     return(
         <>
+            <h1>{status}</h1>
             <div className="linha">
                 <Quadrado value={quadrados[0]} onQuadrado={()=> handleClick(0)} />
                 <Quadrado value={quadrados[1]} onQuadrado={()=> handleClick(1)} />
@@ -44,6 +60,28 @@ function Tabuleiro () {
             </div>
         </>
     )
+}
+
+function Vencedor (quadrados) {
+    const linhas = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < linhas.length; i++) {
+        const [a, b, c] = linhas[i];
+ 
+        if (quadrados[a] && quadrados[a] === quadrados[b] && quadrados[a] === quadrados[c]) {
+            return quadrados[a];
+        }
+    }
+
+    return null;
 }
 
 export default Tabuleiro;
